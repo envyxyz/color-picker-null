aca7d81fec882c09a709a20efda8d3510ede8b52
envyxyz <80117947+envyxyz@users.noreply.github.com>
2022-03-30 20:44:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let span = document.getElementById('result');
+let span = document.getElementById('result');
 let c1 = document.getElementById('c1');
 let c2 = document.getElementById('c2');
 let c3 = document.getElementById('c3');
@@ -8,19 +8,19 @@ let variables = document.documentElement;
 
 c1.addEventListener("input", function() {
     let c1V = c1.value;
-//     span.textContent = c1V;
+    span.textContent = c1V;
     variables.style.setProperty('--text', c1V);
 }, false);
 
 c2.addEventListener("input", function() {
     let c2V = c2.value;
-//     span.textContent = c2V;
+    span.textContent = c2V;
     variables.style.setProperty('--bg', c2V);
 }, false);
 
 c3.addEventListener("input", function() {
     let c3V = c3.value;
-//     span.textContent = c3V;
+    span.textContent = c3V;
     variables.style.setProperty('--sub-bg', c3V);
 }, false);
 
